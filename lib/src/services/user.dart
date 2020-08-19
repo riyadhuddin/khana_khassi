e5be174c7d3d63b211856102a28b815555cf2c5f
Riyadh Uddin <13874736+riyadhuddin@users.noreply.github.com>
2020-08-19 20:31:12 +0600

    category and user service updated

--- lib/src/services/user.dart
+++ lib/src/services/user.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:khana_khassi/src/models/cart_item.dart';
 import 'package:khana_khassi/src/models/user.dart';
 
 class UserServices {
@@ -12,6 +13,22 @@ class UserServices {
 
   void updateUser(Map<String, dynamic> values) {
     _firestore.collection(collection).document(values['id']).updateData(values);
+  }
+
+  void addToCart({String userId, CartItemModel cartItem}) {
+    print("The user id is: $userId");
+    print("cart items are: ${cartItem.toString()}");
+    _firestore.collection(collection).document(userId).updateData({
+      "cart": FieldValue.arrayUnion([cartItem.toMap()])
+    });
+  }
+
+  void removeFromCart({String userId, CartItemModel cartItem}) {
+    print("The user id is: $userId");
+    print("cart items are: ${cartItem.toString()}");
+    _firestore.collection(collection).document(userId).updateData({
+      "cart": FieldValue.arrayRemove([cartItem.toMap()])
+    });
   }
 
   Future<UserModel> getUserById(String id) =>
