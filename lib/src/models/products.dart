eb31a97d87c7bf522d6bc6412663fa445dce464e
Riyadh Uddin <13874736+riyadhuddin@users.noreply.github.com>
2020-08-19 18:21:35 +0600

    models correction

--- lib/src/models/products.dart
+++ lib/src/models/products.dart
@@ -1,16 +1,4 @@
-class Product {
-  final String name;
-  final String image;
-  final double price;
-  final double rating;
-  final String vendor;
-  final bool wishList;
-
-  Product(
-      {this.name,
-      this.image,
-      this.price,
-      this.rating,
-      this.vendor,
-      this.wishList});
+import 'package:cloud_firestore/cloud_firestore.dart';
+class ProductModel {
+ static const ID = "id";
 }
